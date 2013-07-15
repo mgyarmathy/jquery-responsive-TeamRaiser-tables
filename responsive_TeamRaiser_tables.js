b3a1998d16247cda2d508831e26b2e89e4490012
Michael Gyarmathy <mgyarmathy@tamu.edu>
2013-07-15 13:10:50 -0500

    updated CSS class names to be more descriptive

--- responsive_TeamRaiser_tables.js
+++ responsive_TeamRaiser_tables.js
@@ -1,7 +1,7 @@
 /*    
 *    responsive_TeamRaiser_tables.js
 *    Author: Michael Gyarmathy, Web Developer Intern, Blackbaud
-*    Date: July 10, 2013
+*    Last Updated: July 15, 2013
 *    Description: uses DOM traversal to extract information from a TR table and generate a new table with a responsive DIV format. 
 *                 also sorts table entries by amount raised. (requires jQuery)
 *                 example here: http://site.wish.org/site/TR?team_id=1011&fr_id=1041&pg=team
@@ -18,17 +18,17 @@
    
     // create heading for table
     $('#teamRosterTable').append( '<div class="tableRow tableHeading">'
-                                +   '<div class="tableItem teamMember">'
-                                +      '<div class="itemContent">Team Member</div>'
+                                +   '<div class="tableCell teamMember">'
+                                +      '<div class="cellContent">Team Member</div>'
                                 +   '</div>'
-                                +   '<div class="tableItem achievements">'
-                                +      '<div class="itemContent">Achievements</div>'
+                                +   '<div class="tableCell achievements">'
+                                +      '<div class="cellContent">Achievements</div>'
                                 +   '</div>'
-                                +   '<div class="tableItem raised">'
-                                +      '<div class="itemContent">Raised</div>'
+                                +   '<div class="tableCell raised">'
+                                +      '<div class="cellContent">Raised</div>'
                                 +   '</div>'
-                                +   '<div class="tableItem donate">'
-                                +      '<div class="itemContent">&nbsp;</div>'
+                                +   '<div class="tableCell donate">'
+                                +      '<div class="cellContent">&nbsp;</div>'
                                 +   '</div>'
                                 + '</div>'
                                 );
@@ -39,32 +39,32 @@
         var n = $(element).find('td')[0].innerHTML;
         var amt = $(element).find('td')[1].innerHTML;
         records.push({ name: n.substring(3,n.length-2), 
-                       amount_str: amt.substring(0,amt.length-3) , 
-                       amount_num: parseFloat(amt.substring(1,amt.length)) 
+                       amount_display: amt.substring(0,amt.length-3) , 
+                       amount: parseFloat(amt.substring(1,amt.length)) 
                     });
     });
 
     // sort records
-    records.sort(function(a,b) {return (b.amount_num - a.amount_num)} );
+    records.sort(function(a,b) {return (b.amount - a.amount)} );
     
     // add sorted records to the table
-    $(records).each(function(i, element) {
+    $(records).each(function(i, participant) {
         $('#teamRosterTable').append( '<div class="tableRow">'
-                                    +    '<div class="tableItem teamMember">'
-                                    +       '<div class="itemContent">' + element.name + '</div>'
+                                    +    '<div class="tableCell teamMember">'
+                                    +       '<div class="cellContent">' + participant.name + '</div>'
                                     +    '</div>'
-                                    +    '<div class="tableItem achievements">'
-                                    +       '<div class="itemContent">'
+                                    +    '<div class="tableCell achievements">'
+                                    +       '<div class="cellContent">'
                                     +          '<img src="http://i.imgur.com/ePB8oVU.png" />'
                                     +          '<img src="http://i.imgur.com/ePB8oVU.png" />'
                                     +          '<img src="http://i.imgur.com/ePB8oVU.png" />'
                                     +       '</div>'
                                     +    '</div>'
-                                    +    '<div class="tableItem raised">'
-                                    +       '<div class="itemContent">' + element.amount_str + '</div>'
+                                    +    '<div class="tableCell raised">'
+                                    +       '<div class="cellContent">' + participant.amount_display + '</div>'
                                     +    '</div>'
-                                    +    '<div class="tableItem donate">'
-                                    +       '<div class="itemContent">'
+                                    +    '<div class="tableCell donate">'
+                                    +       '<div class="cellContent">'
                                     +          '<button>Donate</button>'
                                     +       '</div>'
                                     +    '</div>'
@@ -73,8 +73,8 @@
     });
 
     // add achievements to each record
-    $('.tableItem.teamMember .itemContent img').each(function(i, element) {
-       $(element).parent().parent().parent().find('.tableItem.achievements .itemContent')
+    $('.tableCell.teamMember .cellContent img').each(function(i, element) {
+       $(element).parent().parent().parent().find('.tableCell.achievements .cellContent')
                  .html($(element).clone());
     });
 
@@ -84,9 +84,9 @@
         captains.push($(element).html());
     });
     $(captains).each(function(i, name) {
-        $('.tableItem.teamMember .itemContent a').each(function(j, element) {
+        $('.tableCell.teamMember .cellContent a').each(function(j, element) {
             if(element.innerHTML == name){
-                $(element).parent().parent().parent().find('.tableItem.achievements .itemContent')
+                $(element).parent().parent().parent().find('.tableCell.achievements .cellContent')
                           .prepend('<img src="http://site.wish.org/images/friendraiser/yellow_star.gif" title="Team Captain" />');
                 return false;
             }
